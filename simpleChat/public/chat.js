aad3465e68abcbc68c6900ef08879e70a5c5ca54
ngquydat <dat.nq@altplus.com.vn>
2018-12-26 14:39:57 +0700

    simpleChat - add feature: login, show viiewer

--- simpleChat/public/chat.js
+++ simpleChat/public/chat.js
@@ -1,42 +1,103 @@
-$(function(){
-	//make connection
-	var socket = io.connect('http://localhost:3000')
+$(function() {
+    // make connection to node server
+    var socket = io.connect('http://localhost:3000')
 
-	//buttons and inputs
-	var message = $("#message")
-	var username = $("#username")
-	var send_message_btn = $("#send_message_btn")
-	var send_username_btn = $("#send_username_btn")
-	var chatroom = $("#chatroom")
-	var feedback = $("#feedback")
+    var username
+    var lastTypingTime
+    var TYPING_TIMER_LENGTH = 400 // ms
+    var connected           = false
+    var typing              = false
 
-	//Emit message
-	send_message_btn.click(function(){
-		console.log("user clicked on 'send' message button")
-		socket.emit('new_message', {message : message.val()})
-	})
+    var $window             = $(window);
+    var $inputMessage       = $("#inputMessage")
+    var $usernameInput      = $('.usernameInput') // Input for username
+    var $currentInput       = $usernameInput.focus()
+    var $loginPage          = $('.login.page') // The login page
+    var $chatPage           = $('.chat.page') // The chatroom page
 
-	//Emit a username
-	send_username_btn.click(function(){
-		console.log("user clicked on 'change username' button")
-		socket.emit('change_username', {username : username.val()})
-	})
+    $loginPage.click(() => {
+        $currentInput.focus()
+    });
 
-	//Emit typing
-	message.bind("keypress", () => {
-		console.log("user typing ...")
-		socket.emit('typing')
-	})
+    // Keyboard events
+    $window.keydown(event => {
+        $currentInput.focus()
+        // When the client hits ENTER on their keyboard
+        if (event.which === 13) {
+            // If the username is valid
+            if (username) {
+                socket.emit('new_message', { message: $inputMessage.val() })
+                socket.emit('stop typing')
+                typing = false;
+            } else {
+                username = $usernameInput.val().trim()
+                if (username) {
+                    $loginPage.fadeOut()
+                    $chatPage.show()
+                    $loginPage.off('click')
+                    $currentInput = $inputMessage.focus()
 
-	//Listen on new_message
-	socket.on("new_message", (data) => {
-		feedback.html('');
-		message.val('');
-		chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
-	})
+                    // Tell the server your username
+                    socket.emit('user join', username)
+                }
+            }
+        }
+    });
 
-	//Listen on typing
-	socket.on('typing', (data) => {
-		feedback.html("<p><i>" + data.username + " is typing a message..." + "</i></p>")
-	})
+    //Emit typing
+    $inputMessage.bind("keypress", () => {
+        if (connected) {
+            if (!typing) {
+                typing = true
+                socket.emit('typing')
+            }
+            lastTypingTime = (new Date()).getTime()
+
+            setTimeout(() => {
+                var typingTimer = (new Date()).getTime()
+                var timeDiff = typingTimer - lastTypingTime
+                if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+                    socket.emit('stop typing')
+                    typing = false
+                }
+            }, TYPING_TIMER_LENGTH)
+        }
+    })
+
+    // Whenever the server emits 'user join', log the user join message
+    socket.on('user join', (data) => {
+        connected = true
+        $('#numUsers').html("participants: " + data.numUsers + " - total viewers: " + data.numGuests);
+        $('#info').html('<p>' + data.username + ' join</p>')
+    })
+
+    // Whenever the server emits 'user join', log the user join message
+    socket.on('guest join', (data) => {
+        $('#numUsers').html("participants: " + data.numUsers + " - total viewers: " + data.numGuests);
+    })
+
+    // Whenever the server emits 'user left', log it in the chat body
+    socket.on('user left', (data) => {
+        $('#info').html('<p>' + data.username + ' left</p>')
+        $('#numUsers').html("participants: " + data.numUsers + " - total viewers: " + data.numGuests);
+    })
+
+    //Listen on new_message
+    socket.on("new_message", (data) => {
+        $inputMessage.val('')
+        $("#messages").append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+        $("#messages")[0].scrollTop = $("#messages")[0].scrollHeight
+    })
+
+    //Listen on typing
+    socket.on('typing', (data) => {
+        $('#info').html("<p class='typing'><i>" + data.username + " is typing a message..." + "</i></p>")
+    })
+
+    // Whenever the server emits 'stop typing', kill the typing message
+    socket.on('stop typing', () => {
+        $('#info .typing').fadeOut(function() {
+            $(this).remove();
+        });
+    });
 });
--- simpleChat/server.js
+++ simpleChat/server.js
@@ -10,39 +10,69 @@ app.use(express.static('public'))
 
 //routes
 app.get('/', (req, res) => {
-	res.render('index')
+    res.render('index')
 })
 
 //Listen on port 3000
 server = app.listen(port, function() {
-    console.log('listening on *:'+port)
+    console.log('listening on *:' + port)
 })
+
+var numGuests = 0
+var numUsers = 0
 
 //socket.io instantiation
 const io = require("socket.io")(server)
 
 //listen on every connection
 io.on('connection', (socket) => {
-	//default username
-	socket.username = "Anonymous"
-	console.log(socket.username+' connected')
+    var joinedUser = false;
 
-    //listen on change_username
-    socket.on('change_username', (data) => {
-    	console.log(socket.username+' has changed name to: '+data.username)
-        socket.username = data.username
-    })
+    //default username
+    socket.username = "Anonymous"
+    console.log(socket.username + ' connected')
+
+    // update guest number
+    ++numGuests
+    io.sockets.emit('guest join', { numUsers: numUsers, numGuests: numGuests })
 
     //listen on new_message
     socket.on('new_message', (data) => {
-    	console.log('Message from '+socket.username+': '+data.message)
+        console.log('Message from ' + socket.username + ': ' + data.message)
         //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username})
+        io.sockets.emit('new_message', { message: data.message, username: socket.username })
     })
 
     //listen on typing
     socket.on('typing', (data) => {
-    	console.log(socket.username+' is typing ...')
-    	socket.broadcast.emit('typing', {username : socket.username})
+        socket.broadcast.emit('typing', { username: socket.username })
+    })
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+        socket.broadcast.emit('stop typing', { username: socket.username })
+    })
+
+    // when the client emits 'user join', this listens and executes
+    socket.on('user join', (username) => {
+        if (joinedUser) return
+        // we store the username in the socket session for this client
+        socket.username = username
+        ++numUsers
+        joinedUser = true
+        socket.emit('user join', { username: socket.username, numUsers: numUsers, numGuests: numGuests })
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit('user join', { username: socket.username, numUsers: numUsers, numGuests: numGuests })
+        console.log(socket.username + " joined")
+    })
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+        if (joinedUser) {
+            --numUsers
+        }
+        --numGuests
+        socket.broadcast.emit('user left', { username: socket.username, numUsers: numUsers, numGuests: numGuests })
+        console.log(socket.username + " left")
     })
 })
