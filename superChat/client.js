fce33d5830d8b862ea80f5c153b96c2f366686cf
ngquydat <dat.nq@altplus.com.vn>
2018-12-27 16:22:11 +0700

    superChat - create basic views

--- superChat/client.js
+++ superChat/client.js
@@ -36,22 +36,25 @@ app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRo
     })
 }]);
 
-app.controller('loginController',['$scope','socket','$http','$state',function($scope,socket,$http,$state){
+app.controller('loginController',['$scope','$http','$state',function($scope,$http,$state){
     console.log('loginController');
-    // Listen currentUser
-    socket.on('currentUser', function(currentUser){
-        $scope.currentUser = currentUser;
-        console.log("Get currentUser : "+$scope.currentUser);
-    });
 
     // call API /register to execute FORM REGISTER
     // call API /login to execute FORM LOGIN
+    $scope.Guest = function() {
+        $state.go('afterLogin');
+    }
 }]);
 
-app.controller('mypageController',['$scope','socket','$http',function($scope,socket,$http){
+app.controller('mypageController',['$scope','socket','$http','$state',function($scope,socket,$http,$state){
     console.log('mypageController');
 
     // Listen currentUser
+    socket.on('currentUser', function(currentUser){
+        $scope.currentUser = currentUser;
+        $scope.$apply();
+        console.log("Get currentUser : "+$scope.currentUser);
+    });
     // Listen userList
     // Listen friendList
     // Listen onlineList
@@ -62,4 +65,7 @@ app.controller('mypageController',['$scope','socket','$http',function($scope,soc
 
     // call API /friend_request to execute FORM FRIEND_REQUEST
     // call API /friend_request/confirm to execute FORM FRIEND_REQUEST_CONFIRM
+    $scope.Logout = function() {
+        $state.go('beforeLogin');
+    }
 }]);
