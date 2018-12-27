75da43849abf0009ea8192a9d240655ab139ae32
ngquydat <dat.nq@altplus.com.vn>
2018-12-27 16:51:33 +0700

    superChat - add setting mongodb

--- superChat/server.js
+++ superChat/server.js
@@ -8,15 +8,32 @@ const port = 3000;
 // Init variables
 var currentUser='Anonymous';
 
-app.use(express.static('./'));
 /*
- * API
+ * Setting Mongodb
+ */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost:27017/superchat", { useNewUrlParser: true });
+mongoose.connection.on('open', function (ref) {
+    console.log('Connected to mongo server.');
+});
+module.exports.user=mongoose.model('User',new Schema({
+    name:String,
+    password: String,
+    email:String,
+    friends:[]
+},{strict: false}));
+
+/*
+ * Setting App API
  * app.post('/', handleTopRequest);
  * app.post('/register', handleRegister);
  * app.post('/login', handleLogin);
  * app.post('/friend_request', handleFriendRequest);  // emit to 'target client': event 'friendConfirm'
  * app.post('/friend_request/confirmed', handleFriendRequestConfirmed); // emit to 'destination client' and 'target client': event 'friendEstablished'
  */
+app.use(express.static('./'));
 app.get('/',function(req,res){
 	res.sendFile(path.resolve(__dirname+"/views/index.html"));
 });
